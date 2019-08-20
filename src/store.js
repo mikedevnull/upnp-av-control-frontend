e03270722ad80450eb25d5fd4877b82c5289b37a
Michael Meier <michael-meier@bluesheep.de>
2019-08-20 13:50:03 +0200

    add a vuex store to manage application state

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import upnpapi from './upnpapi.js'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  actions: {
+  }
+})
+
+export default store
