cd2443a74b57cdbe4413294f6221a24dcb987cac
Michael Meier <michael-meier@bluesheep.de>
2019-08-22 13:43:45 +0200

    add media server device list to library ui

---
+++ src/components/MediaLibrary.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<div>
+			<v-btn icon @click="selectedMediaserver=undefined">
+				<v-icon large>mdi-home</v-icon>
+			</v-btn>
+		</div>
+		<MediaLibraryDeviceList v-if="selectedMediaserver === undefined" v-on:onSelectMediaserver="onSelectMediaserver"></MediaLibraryDeviceList>
+		<div v-else>TODO: Browse device content</div>
+	</div>
+</template>
+
+<script>
+import MediaLibraryDeviceList from './MediaLibraryDeviceList.vue'
+
+export default {
+	components: {
+		MediaLibraryDeviceList,
+	},
+	data: () => ({
+		selectedMediaserver: undefined
+	}),
+	methods: {
+		onSelectMediaserver: function(value) {
+			this.selectedMediaserver = value
+		}
+	},
+	computed: {
+	}
+};
+</script>
---
+++ src/components/MediaLibraryDeviceList.vue
@@ -0,0 +1,34 @@
+<template>
+	<v-list>
+		<v-subheader>Mediaservers</v-subheader>
+		<v-list-item-group v-model="selectedServer" color="primary" >
+			<v-list-item
+				v-for="server in available_servers"
+				:key="server.udn"
+				:value="server"
+				v-on:click.native="selectServer"
+			>
+				<v-list-item-content>
+					<v-list-item-title v-text="server.name"></v-list-item-title>
+				</v-list-item-content>
+			</v-list-item>
+		</v-list-item-group>
+	</v-list>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+	data: () => ({
+		selectedServer: null
+	}),
+	methods: {
+		selectServer: function() {
+			this.$emit('onSelectMediaserver', this.selectedServer)
+		}
+	},
+	computed: {...mapState(['available_servers']),
+	}
+};
+</script>
