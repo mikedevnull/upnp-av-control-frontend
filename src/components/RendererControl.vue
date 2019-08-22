c7ca214f2bf386a8d84fb15a996650406fa45e76
Michael Meier <michael-meier@bluesheep.de>
2019-08-22 13:36:58 +0200

    disable rendering controls when no player is currently selected/active

--- src/components/RendererControl.vue
+++ src/components/RendererControl.vue
@@ -2,23 +2,23 @@
   <v-card>
     <v-card-text>
       <v-toolbar flat>
-        <v-btn  text icon>
+        <v-btn :disabled="is_disabled" text icon>
           <v-icon large>mdi-volume-high</v-icon>
         </v-btn>
 
-        <v-slider style="padding-top: 20px" v-model="volume" max="100" step="1"></v-slider>
+        <v-slider style="padding-top: 20px" :disabled="is_disabled" v-model="volume" max="100" step="1"></v-slider>
         <p style="min-width: 3em; margin-left: 5px; padding-top: 10px">{{volume}}%</p>
         <v-spacer></v-spacer>
-        <v-btn outlined fab small class="ma-2" color="indigo">
+        <v-btn outlined fab small class="ma-2" color="indigo" :disabled="is_disabled">
           <v-icon large>mdi-skip-previous</v-icon>
         </v-btn>
-        <v-btn outlined fab small class="ma-2" color="indigo">
+        <v-btn outlined fab small class="ma-2" color="indigo" :disabled="is_disabled">
           <v-icon large>mdi-stop</v-icon>
         </v-btn>
-        <v-btn outlined fab class="ma-2" color="indigo">
+        <v-btn outlined fab class="ma-2" color="indigo" :disabled="is_disabled">
           <v-icon large>mdi-play</v-icon>
         </v-btn>
-        <v-btn outlined fab small class="ma-2" color="indigo">
+        <v-btn outlined fab small class="ma-2" color="indigo" :disabled="is_disabled">
           <v-icon large>mdi-skip-next</v-icon>
         </v-btn>
         <v-spacer></v-spacer>
@@ -34,9 +34,13 @@ import ControlPoint from '../upnpapi.js'
 export default {
   data: () => ({ }),
   computed: {
+    is_disabled: function() {
+      return this.$store.state.active_player === null
+           },
     volume: { 
       get: function() { return this.$store.state.volume },
       set: function(value) { 
+        if(!this.is_disabled) {
         // Assume the volume can be set to provide a smooth use experience
         // will be reset by the async updates / event bus if value is different
         this.$store.commit('set_volume', value)
@@ -44,5 +48,6 @@ export default {
       } 
     }
   }
+}
 };
 </script>
