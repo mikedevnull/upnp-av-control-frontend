bb5daed897c192543ce16b3c112263dd4fe54192
Michael Meier <michael-meier@bluesheep.de>
2020-04-18 19:03:25 +0200

    use layout grid for media browser layout

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
   <div class="mdc-typography">
     <TheAppBar></TheAppBar>
     <main class="mdc-top-app-bar--fixed-adjust">
-      <router-link :to="{name: 'media'}">Media</router-link>
-      <router-view></router-view>
+      <div class="main-grid">
+        <router-view></router-view>
+      </div>
     </main>
   </div>
 </template>
@@ -13,24 +14,35 @@ import TheAppBar from "./components/TheAppBar";
 
 export default {
   name: "App",
-  components: { TheAppBar },
-  data: () => ({
-    drawer: null
-
-    //
-  })
+  components: { TheAppBar }
 };
 </script>
 <style lang="scss">
 @use "@material/typography/mdc-typography";
+@use '@material/layout-grid';
 @import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500");
 @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
 body {
   margin: 0;
 }
+.main-grid {
+  @include layout-grid.layout-grid(
+    phone,
+    map-get(layout-grid.$default-margin, phone)
+  );
 
-.main-content {
-  max-width: 1024px;
-  margin: auto;
+  @media (min-width: layout-grid.breakpoint-min(tablet)) {
+    @include layout-grid.layout-grid(
+      tablet,
+      map-get(layout-grid.$default-margin, tablet)
+    );
+  }
+  @media (min-width: layout-grid.breakpoint-min(desktop)) {
+    @include layout-grid.layout-grid(
+      desktop,
+      map-get(layout-grid.$default-margin, desktop),
+      1024px
+    );
+  }
 }
 </style>
--- src/pages/MediaserverBrowser/GenericContainerBrowser.vue
+++ src/pages/MediaserverBrowser/GenericContainerBrowser.vue
@@ -1,24 +1,23 @@
 <template>
-  <div>
-    <LoadSpinner v-if="!ready"></LoadSpinner>
+  <div class="browser-view">
+    <LoadSpinner v-if="!ready" class="mdc-layout-grid__cell--span-12"></LoadSpinner>
     <template v-else>
-      <div class="browser-view">
-        <div class="browser-view__details">
-          <img class="browser-view__artwork" :src="folderIcon" />
-          <span class="mdc-typography--headline5">{{title}}</span>
-        </div>
-        <div class="browser-view__children">
-          <ul class="mdc-list">
-            <li class="mdc-list-item" tabindex="0" v-for="child in children" :key="child.id">
-              <span class="mdc-list-item__graphic">
-                <img class="mdc-image-list__image" :src="iconForItem(child)" />
-              </span>
-              <router-link :to="itemBrowseChildrenRoute(udn, child.id)">
-                <span class="mdc-list-item__text">{{child.title}}</span>
-              </router-link>
-            </li>
-          </ul>
-        </div>
+      <div class="browser-view__details">
+        <img class="browser-view__artwork" :src="folderIcon" />
+        <span class="browser-view-typography--title">{{item.title}}</span>
+        <span class="browser-view-typography--subtitle">{{title}}</span>
+      </div>
+      <div class="browser-view__children">
+        <ul class="mdc-list">
+          <li class="mdc-list-item" tabindex="0" v-for="child in children" :key="child.id">
+            <span class="mdc-list-item__graphic">
+              <img class="mdc-image-list__image" :src="iconForItem(child)" />
+            </span>
+            <router-link :to="itemBrowseChildrenRoute(udn, child.id)">
+              <span class="mdc-list-item__text">{{child.title}}</span>
+            </router-link>
+          </li>
+        </ul>
       </div>
     </template>
   </div>
